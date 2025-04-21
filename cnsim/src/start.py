933ec2c5aef5a37e6287ec7bf55fbe601e80d6ec
wwiras <wwiras@gmail.com>
2025-04-21 15:48:18 +0800

    update start.py to use pod_ip

--- cnsim/src/start.py
+++ cnsim/src/start.py
@@ -4,21 +4,13 @@ import time
 import socket
 import gossip_pb2
 import gossip_pb2_grpc
-from kubernetes import client, config
-
-def get_pod_ip(pod_name, namespace="default"):
-    """Fetches the IP address of a pod in the specified namespace."""
-    config.load_incluster_config()
-    v1 = client.CoreV1Api()
-    pod = v1.read_namespaced_pod(name=pod_name, namespace=namespace)
-    return pod.status.pod_ip
 
 def send_message_to_self(message):
     """Sends a message to the current pod (itself)."""
     pod_name = socket.gethostname()
-    print(f"pod_name={pod_name}", flush=True)
-    pod_ip = get_pod_ip(pod_name)
-    print(f"pod_ip={pod_ip}", flush=True)
+    print(f"pod_name: {pod_name}", flush=True)
+    pod_ip = socket.gethostbyname(pod_name)
+    print(f"pod_ip: {pod_ip}", flush=True)
     target = f"{pod_ip}:5050"
     print(f"target={target}", flush=True)
     target_latency = 0.00
@@ -28,7 +20,7 @@ def send_message_to_self(message):
         print(f"Sending message to self ({pod_name}, {pod_ip}): '{message}' with latency={target_latency} ms", flush=True)
         response = stub.SendMessage(gossip_pb2.GossipMessage(
             message=message,
-            sender_id=pod_name,
+            sender_id=pod_ip,
             timestamp=time.time_ns(),
             latency_ms=target_latency
         ))
