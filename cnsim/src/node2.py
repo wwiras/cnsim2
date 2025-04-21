a38677580d61f0c053aa28d7770358a8282fe25b
wwiras <wwiras@gmail.com>
2025-04-21 11:53:46 +0800

    update grpc, docker and topology v1

--- cnsim/src/node2.py
+++ cnsim/src/node2.py
@@ -18,42 +18,15 @@ class Node(gossip_pb2_grpc.GossipServiceServicer):
         self.port = '5050'
         self.service_name = service_name
         self.app_name = 'bcgossip'
-        self.filename = os.environ['FILENAME']
-        # Get topology detail from json file
-        self.topology = self.get_topology('topology')
         # List to keep track of IPs of neighboring nodes
         self.susceptible_nodes = []
         # Set to keep track of messages that have been received to prevent loops
         self.received_message = ""
         # self.gossip_initiated = False
 
-    def get_topology(self,topology_folder):
-        """
-        Reads and returns the content of a specific JSON file from the topology directory.
-
-        Args:
-            topology_folder: The name of the folder containing the topology files.
-            filename: The exact name of the JSON topology file to read.
-
-        Returns:
-            A dictionary representing the loaded JSON data, or None if the file is not found.
-        """
-        current_directory = os.getcwd()
-        topology_dir = os.path.join(current_directory, topology_folder)
-        topology_file_path = os.path.join(topology_dir, self.filename)
-
-        if os.path.exists(topology_file_path) and os.path.isfile(topology_file_path):
-            try:
-                with open(topology_file_path, 'r') as f:
-                    return json.load(f)
-            except json.JSONDecodeError:
-                print(f"Error decoding JSON from file: {topology_file_path}")
-                return None
-        else:
-            print(f"Topology file not found: {topology_file_path}")
-            return None
-
     def get_neighbours(self):
+        # Clear the existing list to refresh it
+        self.susceptible_nodes = []
 
         # Load in-cluster config (for running inside Kubernetes)
         config.load_incluster_config()
@@ -65,41 +38,28 @@ class Node(gossip_pb2_grpc.GossipServiceServicer):
         namespace = "default"  # Replace with your namespace if different
         label_selector = f"app={self.app_name}"  # Use the correct label key and value
 
+        # Debugging: Print the namespace and label selector
+        # print(f"Fetching Pods in namespace: {namespace}, with label selector: {label_selector}", flush=True)
+
         try:
-            # First, we need to get all pod names and IPs
             # Fetch Pods in the specified namespace with the label selector
             ret = v1.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
 
-            # temporary of all nodes (name, ip) except own IP addr
-            all_pods = [(pod.metadata.name, pod.status.pod_ip) for pod in ret.items if self.host != pod.status.pod_ip]
+            # Debugging: Print the number of Pods returned
+            # print(f"Number of Pods returned: {len(ret.items)}", flush=True)
 
-            # Second, pick only neighbors that have edge with current pod/nodes
-            # This step will refer to json network topology the we've obtained using
-            # get_topology function previously
+            # Iterate through the Pods
+            for pod in ret.items:
+                # Debugging: Print Pod details
+                # print(f"Pod Name: {pod.metadata.name},
+                # Pod IP: {pod.status.pod_ip}, Labels: {pod.metadata.labels}",flush=True)
 
-            latency_option = os.getenv('LATENCY_OPTION', 'weight')  # Default to 'weight'
-
-            # Clear the existing list to refresh it
-            self.susceptible_nodes = []
-
-            # get neighbor pod's name and IPs from the all_pods list
-            for edge in self.topology['edges']:
-                neighbor_name = None
-                neighbor_ip = None
-                neighbor_latency = None
-
-                if edge['source'] == self.hostname:
-                    neighbor_name = edge['target']
-                    neighbor_latency = edge.get(latency_option)
-                elif edge['target'] == self.hostname:
-                    neighbor_name = edge['source']
-                    neighbor_latency = edge.get(latency_option)
-
-                if neighbor_name:
-                    for pod_name, pod_ip in all_pods:
-                        if pod_name == neighbor_name:
-                            self.susceptible_nodes.append((pod_name, pod_ip, neighbor_latency))
-                            break
+                # Skip the Pod's own IP
+                if self.host == pod.status.pod_ip:
+                    # print(f"Skipping own IP: {self.host}", flush=True)
+                    continue
+                # Add the Pod's IP and name to the list of susceptible nodes
+                self.susceptible_nodes.append((pod.metadata.name, pod.status.pod_ip))
 
             # Optional: Log the list of neighbors for debugging
             # print(f"Susceptible nodes: {self.susceptible_nodes}", flush=True)
@@ -127,39 +87,35 @@ class Node(gossip_pb2_grpc.GossipServiceServicer):
             self.gossip_message(message, sender_id)
             return gossip_pb2.Acknowledgment(details=f"Done propagate! {self.host} received: '{message}'")
 
-        # Check whether the message is already received or no
+        # Check whether the message is already received or not
         # Notify whether accept it or ignore it
         elif self.received_message == message:
             log_message = f"{self.host} ignoring duplicate message: {message} from {sender_id}"
             self._log_event(message, sender_id, received_timestamp, None, 'duplicate', log_message)
             return gossip_pb2.Acknowledgment(details=f"Duplicate message ignored by ({self.host})")
-        # Distribute gossip
-        # Once received, update message and distribute to connected neighbors
         else:
             self.received_message = message
             propagation_time = (received_timestamp - request.timestamp) / 1e6
             log_message = (f"({self.hostname}({self.host}) received: '{message}' from {sender_id}"
                            f" in {propagation_time:.2f} ms ")
             self._log_event(message, sender_id, received_timestamp, propagation_time, 'received', log_message)
-            self.gossip_message(message, sender_id) # key distribution
+            # Start gossip only when the node is the gossip initiator itself
+            # therefore, only one iteration is required
             return gossip_pb2.Acknowledgment(details=f"{self.host} received: '{message}'")
 
     def gossip_message(self, message, sender_ip):
         # Refresh list of neighbors before gossiping to capture any changes
         if len(self.susceptible_nodes) == 0:
             self.get_neighbours()
-        print(f"self.susceptible_nodes: {self.susceptible_nodes}", flush=True)
+        print(f"self.susceptible_nodes: {self.susceptible_nodes}",flush=True)
 
         # print(f"self.susceptible_nodes={self.susceptible_nodes}",flush=True)
-        for peer_name, peer_ip, neighbor_latency in self.susceptible_nodes:
+        for peer_name, peer_ip in self.susceptible_nodes:
             # Exclude the sender from the list of nodes to forward the message to
             if peer_ip != sender_ip:
 
                 # Record the send timestamp
                 send_timestamp = time.time_ns()
-
-                # Simulate latency
-                time.sleep(float(neighbor_latency) / 1000)
 
                 with grpc.insecure_channel(f"{peer_ip}:5050") as channel:
                     try:
@@ -168,7 +124,6 @@ class Node(gossip_pb2_grpc.GossipServiceServicer):
                             message=message,
                             sender_id=self.host,
                             timestamp=send_timestamp,
-                            latency_ms=neighbor_latency  # Include latency in the gRPC message
                         ))
                     except grpc.RpcError as e:
                         print(f"Failed to send message: '{message}' to {peer_ip}: {e}", flush=True)
